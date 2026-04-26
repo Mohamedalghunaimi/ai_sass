3cfe54511b1f3500d6678d2bc423d32699cf0522
Mohamed Nabil <mohamed@email.com>
2026-04-26 15:17:31 +0300

    updated

--- src/auth/guards/at-guard/at-guard.guard.ts
+++ src/auth/guards/at-guard/at-guard.guard.ts
@@ -1,6 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 /* eslint-disable prettier/prettier */
-import {   Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
+
 @Injectable()
-export class AtGuard extends AuthGuard('at-jwt') {}
+export class AtGuard extends AuthGuard('at-jwt') {
+    constructor(private reflector: Reflector){
+        super()
+    }
+    canActivate(context: ExecutionContext) {
+        const isPublic = this.reflector.getAllAndOverride('is-public', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+    
+        if (isPublic) return true;
+    
+        return super.canActivate(context);
+    }
+}
